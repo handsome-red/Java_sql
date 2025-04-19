aac87f3d2f3dd141abeff7d0553167e2acec8768
gabrilau <egorovandrey314@gmail.com>
2025-04-19 13:16:50 +0300

    1

---
+++ script.sh
@@ -0,0 +1,4 @@
+#psql -U <user_name> -d <data_base_name>
+psql -U atlas -d postgres
+
+CREATE DATABASE university;
---
+++ university.sql
@@ -0,0 +1,33 @@
+-- Создание таблицы преподавателей
+CREATE TABLE teachers (
+    teacher_id INT PRIMARY KEY SERIAL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    middle_name VARCHAR(50)
+);
+
+-- Создание таблицы курсов
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY SERIAL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    teacher_id INT NOT NULL,
+    FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id)
+);
+
+-- Создание таблицы студентов
+CREATE TABLE students (
+    student_id INT PRIMARY KEY SERIAL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    middle_name VARCHAR(50)
+);
+
+-- Создание таблицы для связи многие-ко-многим между студентами и курсами
+CREATE TABLE student_courses (
+    student_id INT NOT NULL,
+    course_id INT NOT NULL,
+    PRIMARY KEY (student_id, course_id),
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
